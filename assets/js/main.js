5490272e8b6f0c715be0b8ff8899eeba1f5452c3
ValLR <valentinalopezrecabal@gmail.com>
2017-06-30 09:10:25 -0400

    modificaciones js, no puedo alinear la x

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){ /*usamos ready para que el jquery sólo cargue cua
 	$("button").click(function(e){
 		e.preventDefault();
 
-		var tasks = $(#add).val();
+		var tasks = $("#add").val();
 
 		//if para validations
 		if(tasks==""){
@@ -11,12 +11,23 @@ $(document).ready(function(){ /*usamos ready para que el jquery sólo cargue cua
 		}
 		else{
 		//si no está vacío el campo, encontes creamos un div para las taread completadas y la casilla/checkbox con lo que se recoge del input
-		$("#tareas").append("<div class='hide-div'><input type='checkbox' id='casilla'><label for='ticket' id='ls'>"+ tasks + "</label><a class='hide-element'>x</a></div>");
+		$("#tasks").append("<div class='hide-div'><input type='checkbox' id='casilla'><label for='ticket' id='ls'>"+ tasks + "</label><a class='hide-element'>x</a></div>");
 		}
 		$('#add').val(""); //<<<-- para limpiar donde ingreso tareas
 
+		$(document).on('click', '.hide-element', function(){
+			$(this).parent().remove();
+		});
 
+		$(document).on('click', '.hide-element', function(){ /*eliminamos tareas clickeando la x*/
+			$(this).parent().remove();
+		});
 
-	})
+		$(document).on('click', '#checkbox', function(){
+			//se elimina el ticket(todo) en el primer nodo y se le agrega al otro nodo.
+			var elimina = $(this).parent().remove();
+			$("#tareas-completadas").append(elimina);
+		});
+	});
 
 })
