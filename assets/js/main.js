f52cf3fb20e16e806b0ce428517507bb03665d82
ValLR <valentinalopezrecabal@gmail.com>
2017-07-04 00:10:18 -0400

    resolviendo problemas con js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){ /*usamos ready para que el jquery sólo cargue cuando a página esté lista*/
+$(document).ready(function(){ /*con ready el jquery sólo carga cuando a página esté lista*/
 
 	$("button").click(function(e){
 		e.preventDefault();
@@ -6,28 +6,27 @@ $(document).ready(function(){ /*usamos ready para que el jquery sólo cargue cua
 		var tasks = $("#add").val();
 
 		//if para validations
-		if(tasks==""){
+		if (tasks=="") {
 			alert("Please write down a task");
 		}
 		else{
 		//si no está vacío el campo, encontes creamos un div para las taread completadas y la casilla/checkbox con lo que se recoge del input
-		$("#tasks").append("<div class='hide-div'><input type='checkbox' id='casilla'><label for='ticket' id='ls'>"+ tasks + "</label><a class='hide-element'>x</a></div>");
+		$("#tasks").append("<div class='hide-div'><input type='checkbox' id='casilla'><label for='casilla' id='ls'>"+ tasks + "</label><a class='hide-element'>x</a></div>");
 		}
 		$('#add').val(""); //<<<-- para limpiar donde ingreso tareas
 
-		$(document).on('click', '.hide-element', function(){
+
+		$(document).on("click", ".hide-element", function(){ /*eliminamos tareas clickeando la x*/
 			$(this).parent().remove();
 		});
 
-		$(document).on('click', '.hide-element', function(){ /*eliminamos tareas clickeando la x*/
-			$(this).parent().remove();
-		});
-
-		$(document).on('click', '#checkbox', function(){
+		$(document).on("click", "#checkbox", function(){
 			//se elimina el ticket(todo) en el primer nodo y se le agrega al otro nodo.
-			var elimina = $(this).parent().remove();
-			$("#tareas-completadas").append(elimina);
+			var erase = $(this).parent().remove();
+			$("#completed-task").append(erase);
 		});
 	});
 
 })
+
+
